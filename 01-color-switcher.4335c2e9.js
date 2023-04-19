65e0a9284e769bb666a3058259cfb55f17b889cc
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-04-19 20:58:18 +0000

    Deploying to gh-pages from @ oleksandrmaliy/goit-js-hw-09@b1324167ed67b1756b1485ebf303287c690558f9 🚀

---
+++ 01-color-switcher.4335c2e9.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.body;function n(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){o.style.backgroundColor=n(),t.disabled=!0,e.disabled=!1,colorizeTimerId=setInterval((function(){o.style.backgroundColor=n()}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(colorizeTimerId)})),e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.4335c2e9.js.map
