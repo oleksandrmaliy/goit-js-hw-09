6d520bb9a7468b2bef7e32ec098ee83774c23d91
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-04-22 11:50:21 +0300

    ??

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,83 +1,84 @@
-// Описаний в документації
-import flatpickr from 'flatpickr';
-// Додатковий імпорт стилів
-import 'flatpickr/dist/flatpickr.min.css';
+// // Описаний в документації
+// import flatpickr from 'flatpickr';
+// // Додатковий імпорт стилів
+// import 'flatpickr/dist/flatpickr.min.css';
 
-import Notiflix from 'notiflix';
+// import Notiflix from 'notiflix';
 
-let ms = 0;
-let x = {};
+// let ms = 0;
+// let x = {};
 
-const inputDateTime = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('button[data-start]');
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+// const inputDateTime = document.querySelector('#datetime-picker');
+// const startBtn = document.querySelector('button[data-start]');
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
 
-    const dateNew = selectedDates[0];
-    const dateNow = options.defaultDate;
+//     const dateNew = selectedDates[0];
+//     const dateNow = options.defaultDate;
 
-    ms = dateNew - dateNow;
+//     ms = dateNew - dateNow;
 
-    if (ms > 0) {
-      startBtn.disabled = false;
-    } else {
-      alert('Choise date in further!');
-    }
+//     if (ms > 0) {
+//       startBtn.disabled = false;
+//     } else {
+//       alert('Choise date in further!');
+//     }
 
-    console.log('');
-    console.log(dateNew);
-    console.log(dateNow);
-    console.log(ms);
-  },
-};
-const fp = flatpickr(inputDateTime, options); // flatpickr
+//     console.log('');
+//     console.log(dateNew);
+//     console.log(dateNow);
+//     console.log(ms);
+//   },
+// };
+// const fp = flatpickr(inputDateTime, options); // flatpickr
 
-startBtn.addEventListener('click', console.log('zzzzzzz'));
-startBtn.disabled = true;
+// startBtn.addEventListener('click',  calculateTime);
+// startBtn.disabled = true;
 
-function calculateTime() {
-  //   console.log(ms);
-  //   console.log(convertMs(ms));
-  //   console.log(convertMs(2000));
-}
+// function calculateTime() {
+//     console.log(ms);
+//     console.log(convertMs(ms));
+//     console.log(convertMs(2000));
+// }
 
-// let days = 0;
-// let hours = 0;
-// let minutes = 0;
+// // let days = 0;
+// // let hours = 0;
+// // let minutes = 0;
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  return (x = { days, hours, minutes, seconds });
-}
+//   return (x = { days, hours, minutes, seconds });
+// }
 
-function addLeadingZero(value) {
-  // padStart();
-  console.log('value');
-}
-convertMs(ms);
-addLeadingZero(convertMs(ms));
-const z = 5;
-function foo(value) {
-  console.log(value.toString.padStart(3, 4));
-}
+// function addLeadingZero(value) {
+//   // padStart();
+//   console.log('value');
+// }
+// convertMs(ms);
+// addLeadingZero(convertMs(ms));
+// const z = 5;
+// function foo(value) {
+//   console.log(value.toString.padStart(3, 4));
+// }
 
-foo(z);
+// foo(z);
+console.log('zzz');
