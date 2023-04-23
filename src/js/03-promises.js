0a0df2ce0e89f928dbe5ff2083fd8a5655f8be7b
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-04-23 15:50:32 +0300

    start task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.success('Sol lucet omnibus');
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+Notiflix.Notify.warning('Memento te hominem esse');
+Notiflix.Notify.info('Cogito ergo sum');
+
+const firstDelay = document.querySelector('[name=delay]');
+const delayStep = document.querySelector('[name=step]');
+const amount = document.querySelector('[name=amount]');
+const submitBtn = document.querySelector('button[type=submit]');
+
+// console.log(firstDelay);
+// console.log(delayStep);
+// console.log(amount);
+// console.log(submitBtn);
+
+submitBtn.addEventListener('click', createPromise);
+
+function foo(event){
+  event.preventDefault();
+  console.log('zxc');
+}
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success('Sol lucet omnibus');// Fulfill
   } else {
-    // Reject
+    Notiflix.Notify.failure('Qui timide rogat docet negare');// Reject
   }
 }
